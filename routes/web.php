4696cd3f6c7470136255f7039c5c93ec16097005
agilbudi <agilbudiprasetyo@gmail.com>
2020-12-14 19:01:29 +0700

    Add project to Repo

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+// Route::get('/posts', function(){
+//     return view('livewire.posts');
+// });
+//Route::get('/post', Posts::class);
+// Route::get('/', function () {
+//     return view('home');
+// })->name('/');
+Route::get('/', [PagesController::class, 'index'])->name('/');
+Route::get('/services', [PagesController::class, 'services'])->name('services');
+Route::get('/about', [PagesController::class, 'about'])->name('about');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
