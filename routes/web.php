6307ccec40adfab2538a426c97a4f7f37e93d264
Firli <firlimoo1@gmail.com>
2020-12-20 12:54:02 +0700

    /posts dah aku benerin

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
-use App\Http\Livewire\Posts\Posts;
+use App\Http\Controllers\Posts;
 
 /*
 |--------------------------------------------------------------------------
